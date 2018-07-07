2f895382f36e3cae4df157eab8d75970f537e0e6
Viacheslav <demchenko_slava_@mail.ru>
2018-07-08 01:06:01 +0300

    Сверстал header и половину main

--- src/App.js
+++ src/App.js
@@ -3,31 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/header.js'
+import Menu from './components/menu.js'
+import Main from './components/main.js'
+
+const url = process.env.PUBLIC_URL + 'img/bg-main.jpg';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top:20px;
+`
+const MainWrapper = styled.main`
+    min-height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className='wrapper'>
-        <SayFullName name='Вячеслав' surname='Демченко' link='https://vk.com/demchenko_vyacheslav' />
-        <SayFullName name='Вася' surname='Иванов' link='https://facebook.com/' />
-        <SayFullName name='Петя' surname='Сидоров' link='https://twitter.com/' />
+      <div className='App'>
+      <HeaderWrapper>
+        <Grid>
+          <Header />
+        </Grid>
+      </HeaderWrapper>
+      <MenuWrapper>
+        <Grid>
+          <Menu />
+        </Grid>
+      </MenuWrapper>
+      <MainWrapper>
+        <Grid>
+          <Main />
+        </Grid>
+      </MainWrapper>
       </div>
+      
     );
   }
-}
-
-
-
-function SayFullName(props) {
-  return (
-      <div>
-      <h1>Мое имя - {props.name}, фамилия - {props.surname}</h1>
-      <a href={props.link}> ссылка на мой профиль</a>
-      </div>
-    )
 }
 
 
